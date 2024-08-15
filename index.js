8ba3625b453bcf39c154186f5c1ebaac4b71c13e
prathiusharun <prathiusharun2000@gmail.com>
2024-08-15 11:58:03 +0530

    installed bcrypt and jsonwebtokens

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-const express = require('express')
-const 
+const express = require('express') //Importing express to create servers and routes
+const jwt =require('jsonwebtoken'); //Importing json for token generation and verification
+const bcrypt = require('bcryptjs');// Importing bcrypt for hashing 
+
+const app = express; //initialized app to express
+
+const users = [];  //in-memory user storage
+
+
