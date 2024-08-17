28c54d645ffdaa31d34815c5eb4f30c36640b522
prathiusharun <prathiusharun2000@gmail.com>
2024-08-17 07:33:59 +0530

    added comment

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require('express') //Importing express to create servers and routes
+const express = require('express'); //Importing express to create servers and routes
 const jwt =require('jsonwebtoken'); //Importing json for token generation and verification
 const bcrypt = require('bcryptjs');// Importing bcrypt for hashing 
 
@@ -8,7 +8,7 @@ const users = [];  //in-memory user storage
 
 // post route for registration of a new user
 
-app.post('/register', function(req,res) {
+app.post('/register', function(req,res) {  //added a post route
     const {username,password}  = req.body; //req.body to receive body of req
-    const password = 
+    const password =     
 })
