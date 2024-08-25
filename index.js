7e5487f4c6d41eb8e937584395f50e703ae656c1
prathiusharun <prathiusharun2000@gmail.com>
2024-08-25 15:22:44 +0530

    erased previous code

--- index.js
+++ index.js
@@ -4,12 +4,7 @@ const bcrypt = require('bcryptjs');// Importing bcrypt for hashing
 
 const app = express(); //initialized app to express
 
-const users = [];  //in-memory user storage, database is not used in this. 
-
-// post route for registration of a new user
-
 app.use(express.static('public'));
-
 
 const recipes = [
     {name:"salad", ingredients:["lettuce","tomato","cucumber"] },
@@ -28,25 +23,4 @@ app.get('/suggest', function(req,res) {
 
 });
 
-// app.get('/about', function(req,res){
-//     res.send("This is the about page");
-
-// });
-
-// app.get('/contact', function(req,res){
-//     res.send("This is the contact page");
-
-// });
-
-
-
-// app.post('/register', function(req,res) {  //added a post route with register as the url
-//     const {username,password}  = req.body; //req.body to receive body of req
-//     const password = bcrpt.     //assigning password, 
-
-
-// })
-
-
-
-app.listen(3000);  //listening to port 3000
+app.listen(3000);  
