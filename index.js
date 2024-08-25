87cdc945bc8ff513f051e24f2df19addf3d24efc
prathiusharun <prathiusharun2000@gmail.com>
2024-08-25 15:00:58 +0530

    added get route to make sure that the server is running

--- index.js
+++ index.js
@@ -8,12 +8,17 @@ const users = [];  //in-memory user storage, database is not used in this.
 
 // post route for registration of a new user
 
-app.post('/register', function(req,res) {  //added a post route with register as the url
-    const {username,password}  = req.body; //req.body to receive body of req
-    const password = bcrpt.     //assigning password, 
-
-
+app.get('/', function(req,res) {
+    res.send("Hello world");
 })
 
+
+
+// app.post('/register', function(req,res) {  //added a post route with register as the url
+//     const {username,password}  = req.body; //req.body to receive body of req
+//     const password = bcrpt.     //assigning password, 
+
+
+// })
 
 app.listen(3000);  //listening to port 3000
